1b88c7cdd616fad436cb8da0c0039b22aa851663
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-06-29 19:01:16 +0530

    using layouts

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use('/', require('./routes'));
