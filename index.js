8e11929f2221cfcb1b66f5b3cbd24e7bfb261664
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-06-29 19:48:12 +0530

    individualy styling the pages

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
 app.use(expressLayouts);
+
+// extract styles and scripts from subpages into the layouts
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// Use express router 
+app.use('/', require('./routes'));
+
+// setup the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use('/', require('./routes'));
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
