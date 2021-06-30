0fe62c471c2fdc8acf092e7fb491f169e7de5955
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-06-30 23:12:37 +0530

    creating sign-up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,8 @@ module.exports.profile = function(req, res){
         title: 'users profile'
     });
 };
+module.exports.signUp = function(req, res){
+    return res.render('sign-up', {
+        title: 'Sign Up'
+    });
+};
--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
+router.get('/sign-up', usersController.signUp);
 module.exports = router;
