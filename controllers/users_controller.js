d534b7dc6f000ea18528e10dcae00b95bacd22c8
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-07-01 01:31:46 +0530

    authentication for sign-up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/user');
 module.exports.profile = function(req, res){
     return res.render('profile', {
         title: 'users profile'
@@ -13,3 +14,19 @@ module.exports.signIn = function(req, res){
         title: 'Sign In'
     });
 };
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding the user'); return;}
+
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating the user while signing up'); return;}
+                
+                return res.redirect('/users/sign-in');
+            })
+        }
+    })
+}
