293977f29beba74d53c4710e11953c181eb10ba8
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-07-01 16:16:20 +0530

    accessing the profile page only when user is sign-in

--- config/passprt-local-strategy.js
+++ config/passprt-local-strategy.js
@@ -33,3 +33,18 @@ passport.deserializeUser(function(id, done){
         return done(null, user);
     })
 })
+passport.checkAuthentication = function(req, res, next){
+// If the user is signed-in then pass on the request to the function(controller's action)
+    if(req.isAuthenticated()){
+        return next();
+    }
+    return res.redirect('/users/sign-in');
+};
+passport.setAuthenticatedUser = function(req, res, next){
+    if(req.isAuthenticated()){
+/* req.user contains current signed in user from session cookie and we are just 
+    sending it to the locals for the views */
+        res.locals.user = req.user;
+    }
+    return next();
+}
