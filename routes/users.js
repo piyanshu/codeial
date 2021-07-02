25e8fb688565ff8da217c3c4dac1d675ac29a2f4
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-07-02 06:39:59 +0530

    creating a sign out

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,5 @@ router.post('/create-session', passport.authenticate(
     'local',
     {failureRedirect: '/users/sign-in'}
 ), usersController.createSession);
+router.get('/sign-out', usersController.destroySession);
 module.exports = router;
