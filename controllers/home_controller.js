bf84c63963d2abdf1665da4dcdbb4de5a512d1ed
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-07-04 01:37:21 +0530

    showing the details of the post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,11 @@
+const Post = require('../models/post');
 module.exports.home = function(req, res){
-    return res.render('home', {
-        title: 'Home'
+// Populate the user of each of post
+    Post.find({}).populate('user').exec(function(err, posts){
+        // if(err){console.log('Error in finding posts');return;}
+        return res.render('home', {
+            title: 'Codeial | Home',
+            posts: posts
+        });
     })
 };
