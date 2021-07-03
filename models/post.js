eaf191d2f1beeed9cba01f1659399c9b38e1a6ab
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-07-03 23:57:59 +0530

    creating a post inside db

---
+++ models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const postSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    }
+}, {
+    timestamps: true
+});
+const Post = new mongoose.model('Post', postSchema);
+module.exports = Post;
