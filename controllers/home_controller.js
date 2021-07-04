41782ff16e345880288c20bca32f42888b26d93a
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-07-04 23:10:23 +0530

    showing posts form only when user is signed-in

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,7 @@ const Post = require('../models/post');
 module.exports.home = function(req, res){
 // Populate the user of each of post
     Post.find({}).populate('user').exec(function(err, posts){
-        // if(err){console.log('Error in finding posts');return;}
+        if(err){console.log('Error in finding posts');return;}
         return res.render('home', {
             title: 'Codeial | Home',
             posts: posts
