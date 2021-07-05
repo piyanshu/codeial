6aaaad753b3d49709ab0e8c88335cb438d267317
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-07-06 01:48:22 +0530

    showing commentsfor every post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,15 @@
 const Post = require('../models/post');
 module.exports.home = function(req, res){
 // Populate the user of each of post
-    Post.find({}).populate('user').exec(function(err, posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         if(err){console.log('Error in finding posts');return;}
         return res.render('home', {
             title: 'Codeial | Home',
