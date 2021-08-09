c0e4e36f1ab6b4e6f11963874c4634d4cc97d559
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-08-09 18:40:04 +0530

    async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,22 +1,26 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = function(req, res){
+module.exports.home = async function(req, res){
 // Populate the user of each of post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: 'Codeial | Home',
-                posts: posts,
-                all_users: users
-            });
-        });    
-    })
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: 'Codeial | Home',
+            posts: posts,
+            all_users: users
+        });
+    }catch(err){
+        console.log('ERROR', err);
+        return;
+    }
 };
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,26 +1,33 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
-module.exports.create = function(req, res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err, post){
-        if(err){console.log('Error in creating a post'); return;}
+module.exports.create = async function(req, res){
+    try{
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+
         return res.redirect('back');
-    })
+    }catch(err){
+        console.log('ERROR', err);
+        return;
+    }
+    
 };
-module.exports.destroy = function(req, res){
-    Post.findById(req.params.id, function(err, post){
-        // .id means converting the object id into string
+module.exports.destroy = async function(req, res){
+    try{
+        let post = await Post.findById(req.params.id)
+
         if(post.user == req.user.id){
             post.remove();
-            Comment.deleteMany({post: req.params.id}, function(err){
-                if(err){console.log('error in deleting the posts');return;}
-                res.redirect('back');
-            })
+            await Comment.deleteMany({post: req.params.id});
+            res.redirect('back');
         }
         else{
             res.redirect('back');
         }
-    });
+    }catch(err){
+        console.log('ERROR', err);
+        return;
+    }
 };
